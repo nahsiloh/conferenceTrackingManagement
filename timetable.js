3706d516d103ce81756a52457966ab82ae2a7042
nahsiloh <lishan1512@gmail.com>
2020-01-08 20:37:42 +0800

    created timetable class

--- timetable.js
+++ timetable.js
@@ -4,11 +4,40 @@ NUMBER_OF_MINS_DURING_MORNING_SESSION = 180;
 MIN_NUMBER_OF_MINS_DURING_AFTERNOON_SESSION = 180;
 MAX_NUMBER_OF_MINS_DURING_AFTERNOON_SESSION = 240;
 
-function timetable(data, hoursAvailable) {
-  let schedule = [];
-  if (hoursAvailable === 0) {
-    return schedule;
+class Timetable {
+  constructor(timeAvailable) {
+    this.scheduledTalks = [];
+    this.remainingTime = timeAvailable;
+  }
+
+  addTalkToTimetable(index) {
+    return this.scheduledTalks.push(
+      talkDurationAndTitleObject[index].titles[0]
+    );
+  }
+
+  removeTalkFromTalkData(index) {
+    talkDurationAndTitleObject[index].titles.shift();
+  }
+
+  createTimetable(talkDurationAndTitleObject) {
+    for (let i = 0; i < talkDurationAndTitleObject.length; i++) {
+      if (this.remainingTime === 0) {
+        break;
+      }
+
+      if (
+        this.remainingTime % talkDurationAndTitleObject[i].duration === 0 &&
+        talkDurationAndTitleObject[i].titles.length > 0
+      ) {
+        this.addTalkToTimetable(i);
+        this.removeTalkFromTalkData(i);
+        this.remainingTime -= talkDurationAndTitleObject[i].duration;
+        this.createTimetable(talkDurationAndTitleObject);
+      }
+    }
+    return this.scheduledTalks;
   }
 }
 
-module.exports = timetable;
+module.exports = Timetable;
--- timetable.test.js
+++ timetable.test.js
@@ -1,8 +1,63 @@
-const timetable = require("./timetable");
-const data = require("./index");
+const Timetable = require("./timetable");
+const FormatData = require("./formatData");
+const { testData } = require("./testData");
 
 describe("Create Timetable", () => {
+  beforeEach(() => {
+    const newData = new FormatData(testData);
+    const testDataArray = newData.convertDataToArray(testData);
+    const uniqueTalkDurationArray = newData.createUniqueTalkDurationArray(
+      testDataArray
+    );
+    const uniqueTalkDurationArraySorted = newData.sortUniqueTalkDurationArray(
+      uniqueTalkDurationArray
+    );
+    return (talkDurationAndTitleObject = newData.createTalkDurationAndTitleObject(
+      uniqueTalkDurationArraySorted,
+      testDataArray
+    ));
+  });
+
   it("should return empty if there are no time slots available", () => {
-    expect(timetable(data, 0)).toStrictEqual([]);
+    const schedule = new Timetable(0);
+    expect(schedule.createTimetable(talkDurationAndTitleObject)).toStrictEqual(
+      []
+    );
+  });
+
+  it("should return one 60min talk when there is only 60mins timeslot available", () => {
+    const schedule = new Timetable(60);
+    expect(schedule.createTimetable(talkDurationAndTitleObject)).toStrictEqual([
+      "Writing Fast Tests Against Enterprise Rails 60min"
+    ]);
+  });
+
+  it("should return two 60min talks when there is 120mins timeslot available", () => {
+    const schedule = new Timetable(120);
+    expect(schedule.createTimetable(talkDurationAndTitleObject)).toStrictEqual([
+      "Writing Fast Tests Against Enterprise Rails 60min",
+      "Communicating Over Distance 60min"
+    ]);
+  });
+
+  it("should return two 60min talks (when there are no more 60min talks), and two 30min talks when there is 180mins timeslot available", () => {
+    const schedule = new Timetable(180);
+    expect(schedule.createTimetable(talkDurationAndTitleObject)).toStrictEqual([
+      "Writing Fast Tests Against Enterprise Rails 60min",
+      "Communicating Over Distance 60min",
+      "Sit Down and Write 30min",
+      "Lua for the Masses 30min"
+    ]);
+  });
+
+  it("should return 2 60min, 1 30min and 2 45min talks when there is 240mins timeslot available", () => {
+    const schedule = new Timetable(240);
+    expect(schedule.createTimetable(talkDurationAndTitleObject)).toStrictEqual([
+      "Writing Fast Tests Against Enterprise Rails 60min",
+      "Communicating Over Distance 60min",
+      "Sit Down and Write 30min",
+      "Overdoing it in Python 45min",
+      "Ruby Errors from Mismatched Gem Versions 45min"
+    ]);
   });
 });
