d465b3f411cd181142aab2d61425050120776d6a
nahsiloh <lishan1512@gmail.com>
2020-01-09 17:09:20 +0800

    added code to excute data

--- index.js
+++ index.js
@@ -1,2 +1,37 @@
 const fs = require("fs");
-module.exports = fs.readFileSync("data.txt", "utf8");
+const talksData = fs.readFileSync("talksData.txt", "utf8");
+
+const Timetable = require("./components/Timetable/Timetable");
+const FormatData = require("./common/formatData");
+
+const newTalksData = new FormatData(talksData);
+const talkDurationAndTitleObject = newTalksData.executeFormatData(talksData);
+
+const track1MorningTimetable = new Timetable(180);
+const printTrack1MorningTimetable = track1MorningTimetable.printTimetable(
+  talkDurationAndTitleObject,
+  "09:00AM"
+);
+
+const track1AfternoonTimetable = new Timetable(240);
+const printTrack1AfternoonTimetable = track1AfternoonTimetable.printTimetable(
+  talkDurationAndTitleObject,
+  "01:00PM"
+);
+
+const track2MorningTimetable = new Timetable(180);
+const printTrack2MorningTimetable = track2MorningTimetable.printTimetable(
+  talkDurationAndTitleObject,
+  "09:00AM"
+);
+
+const track2AfternoonTimetable = new Timetable(240);
+const printTrack2AfternoonTimetable = track2AfternoonTimetable.printTimetable(
+  talkDurationAndTitleObject,
+  "01:00PM"
+);
+
+console.log(printTrack1MorningTimetable);
+console.log(printTrack1AfternoonTimetable);
+console.log(printTrack2MorningTimetable);
+console.log(printTrack2AfternoonTimetable);
