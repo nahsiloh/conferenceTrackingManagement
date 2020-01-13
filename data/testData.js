54129376cec1c61a18036be2ecda37d9f4e046bc
nahsiloh <lishan1512@gmail.com>
2020-01-13 16:23:18 +0800

    changed test data

--- data/testData.js
+++ data/testData.js
@@ -1,5 +1,10 @@
 module.exports.testData =
-  "Overdoing it in Python 45min\nSit Down and Write 30min\nWriting Fast Tests Against Enterprise Rails 60min\nLua for the Masses 30min\nRuby Errors from Mismatched Gem Versions 45min\nRails for Python Developers lightning\nCommunicating Over Distance 60min";
+  "Overdoing it in Python 45min\n" +
+  "Sit Down and Write 30min\n" +
+  "Writing Fast Tests Against Enterprise Rails 60min\n" +
+  "Lua for the Masses 30min\nRuby Errors from Mismatched Gem Versions 45min\n" +
+  "Rails for Python Developers lightning\n" +
+  "Communicating Over Distance 60min";
 
 module.exports.testDataArray = [
   "Overdoing it in Python 45min",
@@ -34,6 +39,90 @@ module.exports.testtalkDurationAndTitleArray = [
   },
   { duration: 5, titles: ["Rails for Python Developers lightning"] }
 ];
+
+module.exports.emptyTestTalkDurationAndTitleArray = [
+  {
+    duration: 60,
+    titles: []
+  },
+  {
+    duration: 45,
+    titles: []
+  },
+  {
+    duration: 30,
+    titles: []
+  },
+  { duration: 5, titles: [] }
+];
+
+module.exports.scheduleForTheDayObject = {
+  track: 1,
+  morningSession: [
+    "09:00AM Writing Fast Tests Against Enterprise Rails 60min",
+    "10:00AM Communicating Over Distance 60min",
+    "11:00AM Rails Magic 60min"
+  ],
+  afternoonSession: [
+    "01:00PM Ruby on Rails: Why We Should Move On 60min",
+    "02:00PM Ruby on Rails Legacy App Maintenance 60min",
+    "03:00PM Lua for the Masses 30min",
+    "03:30PM Overdoing it in Python 45min",
+    "04:15PM Ruby Errors from Mismatched Gem Versions 45min",
+    "05:00PM Networking Event"
+  ]
+};
+
+module.exports.scheduleForTheConferenceArray = [
+  {
+    track: 1,
+    morningSession: [
+      "09:00AM Writing Fast Tests Against Enterprise Rails 60min",
+      "10:00AM Communicating Over Distance 60min",
+      "11:00AM Rails Magic 60min"
+    ],
+    afternoonSession: [
+      "01:00PM Ruby on Rails: Why We Should Move On 60min",
+      "02:00PM Ruby on Rails Legacy App Maintenance 60min",
+      "03:00PM Lua for the Masses 30min",
+      "03:30PM Overdoing it in Python 45min",
+      "04:15PM Ruby Errors from Mismatched Gem Versions 45min",
+      "05:00PM Networking Event"
+    ]
+  },
+  {
+    track: 2,
+    morningSession: [
+      "09:00AM Common Ruby Errors 45min",
+      "09:45AM Accounting-Driven Development 45min",
+      "10:30AM Pair Programming vs Noise 45min",
+      "11:15AM Clojure Ate Scala (on my project) 45min"
+    ],
+    afternoonSession: [
+      "01:00PM Woah 30min",
+      "01:30PM Sit Down and Write 30min",
+      "02:00PM Programming in the Boondocks of Seattle 30min",
+      "02:30PM Ruby vs. Clojure for Back-End Development 30min",
+      "03:00PM A World Without HackerNews 30min",
+      "03:30PM User Interface CSS in Rails Apps 30min",
+      "04:00PM Rails for Python Developers lightning",
+      "04:05PM Networking Event"
+    ]
+  }
+];
+
+module.exports.printScheduleForOneTrack =
+  "Track1\n" +
+  "09:00AM Writing Fast Tests Against Enterprise Rails 60min\n" +
+  "10:00AM Communicating Over Distance 60min\n" +
+  "11:00AM Rails Magic 60min\n" +
+  "12:00PM Lunch\n" +
+  "01:00PM Ruby on Rails: Why We Should Move On 60min\n" +
+  "02:00PM Ruby on Rails Legacy App Maintenance 60min\n" +
+  "03:00PM Lua for the Masses 30min\n" +
+  "03:30PM Overdoing it in Python 45min\n" +
+  "04:15PM Ruby Errors from Mismatched Gem Versions 45min\n" +
+  "05:00PM Networking Event\n";
 
 module.exports.printSchedule =
   "Track1\n" +
