ed39c6cd46ff7635a58653adf173b36f3af6571a
nahsiloh <lishan1512@gmail.com>
2020-01-13 16:24:24 +0800

    added logic to determine number of tracks

--- components/ConferenceSchedule/ConferenceSchedule.js
+++ components/ConferenceSchedule/ConferenceSchedule.js
@@ -1,51 +1,139 @@
 const { conferenceTalkSessions } = require("../../data/conferenceTalkSessions");
+const { durationRegex, meridiamRegex } = require("../../common/regex");
+const { formatMinutes, formatHours } = require("../../common/formatTime");
+
 const Timetable = require("../Timetable/Timetable");
+const AFTERNOON_SESSION_START_TIME = "01:00PM";
+const DURATION_OF_LIGHTNING_TALK = 5;
 
 class ConferenceSchedule {
-  constructor(conferenceDays) {
-    this.conferenceDays = conferenceDays;
+  constructor(talkDurationAndTitleArray) {
+    this.track = 1;
+    this.talkDurationAndTitleArray = talkDurationAndTitleArray;
+    this.isUnscheduledTalks = false;
+    this.scheduleForADay = {
+      track: this.track,
+      morningSession: [],
+      afternoonSession: []
+    };
+    this.scheduleForConference = [];
   }
 
-  getScheduledTalks(talkDurationAndTitleObject) {
-    const track1MorningTimetable = new Timetable(
+  checkIfTalkIsLightning(talk) {
+    const durationOfTalk =
+      talk.match(durationRegex) === null
+        ? DURATION_OF_LIGHTNING_TALK
+        : Number(talk.match(durationRegex)[0]);
+    return durationOfTalk;
+  }
+
+  minutesIsMoreThan60(mins) {
+    return mins >= 60;
+  }
+
+  firstTalkOfTheSession(index) {
+    return index === 0;
+  }
+
+  isAfternoonSession(startTime) {
+    return startTime === AFTERNOON_SESSION_START_TIME;
+  }
+
+  assignTimingsToTalks(
+    talkDurationAndTitleArray,
+    startTime,
+    durationOfAvailableTimeForTalks
+  ) {
+    const timetableForASession = new Timetable(durationOfAvailableTimeForTalks);
+    const arrayOfTalksForTimeSlot = timetableForASession.createArrayOfTalksForTimeSlot(
+      talkDurationAndTitleArray
+    );
+
+    const startTimeArray = startTime.match(durationRegex);
+    const currentTimeMeridiem = startTime.match(meridiamRegex);
+
+    let currentTimeHour = Number(startTimeArray[0]);
+    let currentTimeMins = Number(startTimeArray[1]);
+
+    let [printTimeHours, printTimeMins] = [currentTimeHour, currentTimeMins];
+
+    const scheduledTalksWithTimingArray = arrayOfTalksForTimeSlot.map(
+      (talk, index) => {
+        let scheduledTalkTime = "";
+        let durationOfTalk;
+
+        this.firstTalkOfTheSession(index)
+          ? (scheduledTalkTime = startTime)
+          : (scheduledTalkTime = `${printTimeHours}:${printTimeMins}${currentTimeMeridiem}`);
+
+        durationOfTalk = this.checkIfTalkIsLightning(talk);
+        currentTimeMins += durationOfTalk;
+
+        if (this.minutesIsMoreThan60(currentTimeMins)) {
+          currentTimeHour += 1;
+          currentTimeMins -= 60;
+        }
+
+        printTimeHours = formatHours(currentTimeHour);
+        printTimeMins = formatMinutes(currentTimeMins);
+
+        return scheduledTalkTime.concat(" ", talk);
+      }
+    );
+
+    if (this.isAfternoonSession(startTime)) {
+      scheduledTalksWithTimingArray.push(
+        `${printTimeHours}:${printTimeMins}${currentTimeMeridiem} Networking Event`
+      );
+    }
+
+    return scheduledTalksWithTimingArray;
+  }
+
+  getScheduledTalksForOneTrack(talkDurationAndTitleArray) {
+    const arrayOfTalksWithAssignedTimingsForMorning = this.assignTimingsToTalks(
+      talkDurationAndTitleArray,
+      conferenceTalkSessions[0].startTime,
       conferenceTalkSessions[0].durationOfAvailableTimeForTalks
     );
-    const printTrack1MorningTimetable = track1MorningTimetable.executeTimetable(
-      talkDurationAndTitleObject,
-      conferenceTalkSessions[0].startTime
-    );
+    this.scheduleForADay.morningSession = arrayOfTalksWithAssignedTimingsForMorning;
 
-    const track1AfternoonTimetable = new Timetable(
+    const arrayOfTalksWithAssignedTimingsForAfternoon = this.assignTimingsToTalks(
+      talkDurationAndTitleArray,
+      conferenceTalkSessions[1].startTime,
       conferenceTalkSessions[1].durationOfAvailableTimeForTalks
     );
-    const printTrack1AfternoonTimetable = track1AfternoonTimetable.executeTimetable(
-      talkDurationAndTitleObject,
-      conferenceTalkSessions[1].startTime
-    );
+    this.scheduleForADay.afternoonSession = arrayOfTalksWithAssignedTimingsForAfternoon;
 
-    const track2MorningTimetable = new Timetable(180);
-    const printTrack2MorningTimetable = track2MorningTimetable.executeTimetable(
-      talkDurationAndTitleObject,
-      "09:00AM"
-    );
+    return this.scheduleForADay;
+  }
 
-    const track2AfternoonTimetable = new Timetable(240);
-    const printTrack2AfternoonTimetable = track2AfternoonTimetable.executeTimetable(
-      talkDurationAndTitleObject,
-      "01:00PM"
-    );
+  checkForUnscheduledTalks(talkDurationAndTitleArray) {
+    for (let i = 0; i < talkDurationAndTitleArray.length; i++) {
+      if (talkDurationAndTitleArray[i].titles.length > 0) {
+        return (this.isUnscheduledTalks = true);
+      }
+    }
+    return this.isUnscheduledTalks;
+  }
 
-    return (
-      "Track1\n" +
-      `${printTrack1MorningTimetable}\n` +
-      "12:00PM Lunch\n" +
-      `${printTrack1AfternoonTimetable}\n` +
-      "\n" +
-      "Track2\n" +
-      `${printTrack2MorningTimetable}\n` +
-      "12:00PM Lunch\n" +
-      `${printTrack2AfternoonTimetable}`
-    );
+  getScheduledTalksForConferenceArray(talkDurationAndTitleArray) {
+    this.checkForUnscheduledTalks(talkDurationAndTitleArray);
+    while (this.isUnscheduledTalks === true) {
+      this.scheduleForADay = {
+        track: this.track,
+        morningSession: [],
+        afternoonSession: []
+      };
+      this.track += 1;
+      const scheduleForADay = this.getScheduledTalksForOneTrack(
+        talkDurationAndTitleArray
+      );
+      this.scheduleForConference.push(scheduleForADay);
+      this.isUnscheduledTalks = false;
+      this.checkForUnscheduledTalks(talkDurationAndTitleArray);
+    }
+    return this.scheduleForConference;
   }
 }
 
--- components/ConferenceSchedule/ConferenceSchedule.test.js
+++ components/ConferenceSchedule/ConferenceSchedule.test.js
@@ -1,13 +1,70 @@
 const ConferenceSchedule = require("./ConferenceSchedule");
+const { conferenceTalkSessions } = require("../../data/conferenceTalkSessions");
+
 const talksData = require("../../index.js");
 const { executeFormatData } = require("../../common/formatData");
-const { printSchedule } = require("../../data/testData");
+const {
+  testtalkDurationAndTitleArray,
+  scheduleForTheDayObject,
+  scheduleForTheConferenceArray,
+  printScheduleForOneTrack,
+  printSchedule,
+  emptyTestTalkDurationAndTitleArray
+} = require("../../data/testData");
+
 describe("Conference Schedule", () => {
-  it("should get the schedule in an array for the day", () => {
-    const testtalkDurationAndTitleArray = executeFormatData(talksData);
-    const techWeek = new ConferenceSchedule();
-    expect(techWeek.getScheduledTalks(testtalkDurationAndTitleArray)).toBe(
-      printSchedule
-    );
+  describe("Assign timings for the talks", () => {
+    it("should output an array of talks with timings for the conference", () => {
+      const techWeek = new ConferenceSchedule();
+      const output = [
+        "09:00AM Writing Fast Tests Against Enterprise Rails 60min",
+        "10:00AM Communicating Over Distance 60min",
+        "11:00AM Sit Down and Write 30min",
+        "11:30AM Lua for the Masses 30min"
+      ];
+      expect(
+        techWeek.assignTimingsToTalks(
+          testtalkDurationAndTitleArray,
+          "09:00AM",
+          180
+        )
+      ).toStrictEqual(output);
+    });
+  });
+
+  describe("check if there are unscheduled talks", () => {
+    it("should return true if there are unscheduled talks", () => {
+      const techWeek = new ConferenceSchedule();
+      expect(
+        techWeek.checkForUnscheduledTalks(testtalkDurationAndTitleArray)
+      ).toBe(true);
+    });
+
+    it("should return false if there are unscheduled talks", () => {
+      const techWeek = new ConferenceSchedule();
+      expect(
+        techWeek.checkForUnscheduledTalks(emptyTestTalkDurationAndTitleArray)
+      ).toBe(false);
+    });
+  });
+
+  describe("Get array of scheduled talks", () => {
+    it("should get the schedule for the day", () => {
+      const testtalkDurationAndTitleArray = executeFormatData(talksData);
+      const techWeek = new ConferenceSchedule();
+      expect(
+        techWeek.getScheduledTalksForOneTrack(testtalkDurationAndTitleArray)
+      ).toStrictEqual(scheduleForTheDayObject);
+    });
+
+    it("should get the schedule for entire conference", () => {
+      const testtalkDurationAndTitleArray = executeFormatData(talksData);
+      const techWeek = new ConferenceSchedule();
+      expect(
+        techWeek.getScheduledTalksForConferenceArray(
+          testtalkDurationAndTitleArray
+        )
+      ).toStrictEqual(scheduleForTheConferenceArray);
+    });
   });
 });
