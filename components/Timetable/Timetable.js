ab67f034f57b8a498e67c19196cad2bd46c89a74
nahsiloh <lishan1512@gmail.com>
2020-01-13 08:00:58 +0800

    moved assignment of timing functions to conference schedule

--- components/Timetable/Timetable.js
+++ components/Timetable/Timetable.js
@@ -9,7 +9,7 @@ class Timetable {
     this.remainingTime = timeAvailable;
   }
 
-  addTalkToTimetable(array, index) {
+  addTalkToTimetableArray(array, index) {
     return this.arrayOfTalksForTimeSlot.push(array[index].titles[0]);
   }
 
@@ -27,76 +27,13 @@ class Timetable {
         this.remainingTime % talkDurationAndTitleArray[i].duration === 0 &&
         talkDurationAndTitleArray[i].titles.length > 0
       ) {
-        this.addTalkToTimetable(talkDurationAndTitleArray, i);
+        this.addTalkToTimetableArray(talkDurationAndTitleArray, i);
         this.removeTalkFromTalkData(talkDurationAndTitleArray, i);
         this.remainingTime -= talkDurationAndTitleArray[i].duration;
         this.createArrayOfTalksForTimeSlot(talkDurationAndTitleArray);
       }
     }
     return this.arrayOfTalksForTimeSlot;
-  }
-
-  assignTimingsForTalks(arrayOfTalksForTimeSlot, startTime) {
-    const startTimeArray = startTime.match(durationRegex);
-    const currentTimeMeridiem = startTime.match(meridiamRegex);
-
-    let currentTimeHour = Number(startTimeArray[0]);
-    let currentTimeMins = Number(startTimeArray[1]);
-
-    let [printTimeHours, printTimeMins] = [currentTimeHour, currentTimeMins];
-
-    const scheduledTalksWithTiming = arrayOfTalksForTimeSlot.map(
-      (talk, index) => {
-        let scheduledTalkTime = "";
-        let durationOfTalk;
-
-        index === 0
-          ? (scheduledTalkTime = startTime)
-          : (scheduledTalkTime = `${printTimeHours}:${printTimeMins}${currentTimeMeridiem}`);
-
-        talk.match(durationRegex) === null
-          ? (durationOfTalk = DURATION_OF_LIGHTNING_TALK)
-          : (durationOfTalk = Number(talk.match(durationRegex)[0]));
-
-        currentTimeMins += durationOfTalk;
-
-        if (currentTimeMins >= 60) {
-          currentTimeHour += 1;
-          currentTimeMins -= 60;
-        }
-
-        printTimeHours = formatHours(currentTimeHour);
-        printTimeMins = formatMinutes(currentTimeMins);
-
-        return scheduledTalkTime.concat(" ", talk);
-      }
-    );
-
-    if (startTime === AFTERNOON_SESSION_START_TIME) {
-      scheduledTalksWithTiming.push(
-        `${printTimeHours}:${printTimeMins}${currentTimeMeridiem} Networking Event`
-      );
-    }
-
-    return scheduledTalksWithTiming;
-  }
-
-  convertScheduledTalksToString(scheduledTalksWithTiming) {
-    return scheduledTalksWithTiming.join("\n");
-  }
-
-  executeTimetable(talkDurationAndTitleArray, startTime) {
-    const arrayOfTalksForTimeSlot = this.createArrayOfTalksForTimeSlot(
-      talkDurationAndTitleArray
-    );
-    const arrayOfScheduledTalksWithTiming = this.assignTimingsForTalks(
-      arrayOfTalksForTimeSlot,
-      startTime
-    );
-    const compiledTimetable = this.convertScheduledTalksToString(
-      arrayOfScheduledTalksWithTiming
-    );
-    return compiledTimetable;
   }
 }
 
